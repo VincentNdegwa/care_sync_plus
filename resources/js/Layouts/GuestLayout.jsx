6a6a453f72e2f9c16a701dabe6ba125c4abbc2a1
VincentNdegwa <ndegwavincent7@gmail.com>
2024-11-10 00:47:38 +0300

    "Refactor AuthenticatedLayout and GuestLayout components, remove unused imports, and update customAxios.js"

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,5 +1,7 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import axiosInstance from "@/customAxios";
+import LoadingUi from "@/Pages/Ui/LoadinUI";
+import { Link, router } from "@inertiajs/react";
 
 export default function GuestLayout({ children }) {
     return (
